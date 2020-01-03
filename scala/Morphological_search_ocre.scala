401dffdfd36b67b1d9b92a36007672b79e1062bb
Neel Smith <nsmith@Technites.local>
2020-01-03 06:46:27 -0500

    Get correct results for morph search

--- scala/Morphological_search_ocre.scala
+++ scala/Morphological_search_ocre.scala
@@ -15,14 +15,40 @@ import edu.holycross.shot.mid.validator._
 import edu.holycross.shot.latin._
 import edu.holycross.shot.latincorpus._
 
-val url = "https://raw.githubusercontent.com/neelsmith/hctexts/master/cex/ocre43k.cex"
+val url = "https://raw.githubusercontent.com/neelsmith/hctexts/master/cex/ric-1-3.cex"
 val corpus = CorpusSource.fromUrl(url, cexHeader = true)
 
-val fstUrl = "https://raw.githubusercontent.com/neelsmith/hctexts/master/workfiles/ocre/ocre-fst.txt"
-val fstLines = Source.fromURL(fstUrl).getLines.toVector
+// use a local file, or retrieve from github:
+val fstFile = "aha/ric1-3-parses.txt"
+val fstLines = Source.fromFile(fstFile).getLines.toVector
 
+/*
+val fstUrl = "https://raw.githubusercontent.com/neelsmith/hctexts/master/workfiles/ric1-3/ric1-3-parses.txt"
+val fstLines = Source.fromURL(fstUrl).getLines.toVector
+*/
 val ocrelat = LatinCorpus.fromFstLines(corpus, Latin24Alphabet, fstLines, strict = false)
 
+ocrelat.size
+
+
+
+ocrelat.tokenLexemeIndex("libertas")
+ocrelat.lexemeConcordance("ls.n26481")
+
+ocrelat.tokens(0).analyses(0).lemmaId
+
+ocrelat.tokens(0).urn
+
+val tokenLemmaMap = ocrelat.tokens.map(t => (t.urn, t.analyses.map(_.lemmaId).distinct))
+
+
+val lexicallyAmbiguous =  tokenLemmaMap.filter(_._2.size > 1)
+lexicallyAmbiguous.isEmpty
+
+val tokenLemmaPair = tokenLemmaMap.filter(_._2.nonEmpty).map{ case (t,v) => (t, v(0))}
+
+val libertas = tokenLemmaPair.filter(_._2 == "ls.n26481")
+libertas.size
 
 // Find coin IDs where a specified lexeme occurs.
 def findLexemeOccurrences(lexemeId: String) : Vector[String] = {
@@ -51,18 +77,7 @@ val libertasCoins = findOccurrences(token, ocrelat)
 
 println("Found " + libertasCoins.size + " coins with legends including a form of 'libertas'")
 
-/*
-val token2 = "liberalitas"
-val liberalitasCoins = findOccurrences(token2, ocrelat)
-println("Found " + liberalitasCoins.size + " coins with legends including a form of 'liberalitas'")
-*/
 
 val aug = findOccurrences("avgvsto", ocrelat)
 println(aug.size)
 //println(ocrelat.tokens.map(_.urn.collapsePassageTo(2).passageComponent).distinct.sorted.mkString("\n"))
-
-val commodus = ocrelat.tokens.filter(_.urn.passageComponent.startsWith("3.com"))
-
-commodus.size
-
-commodus.filter(_.urn.passageComponent.startsWith("3.com.171"))
